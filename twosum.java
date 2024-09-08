2abccbed981474197538844fa449fee75145735f
Satyam Singh <satyamsinghrajput9980@gmail.com>
2024-09-08 10:25:54 +0530

    arrays two sum.java

---
+++ twosum.java
@@ -0,0 +1,19 @@
+# DSA---Arrays-Leetcode- 
+
+1) Two Sum Problem Leetcode Solution in java
+
+   class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{}; // No solution found
+    }
+} 
+
+2)
